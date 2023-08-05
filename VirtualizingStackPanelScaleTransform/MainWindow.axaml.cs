a0db44bcd97b73589db9762355388191f5aa71d3
SKProCH <skproch@yandex.ru>
2023-08-06 02:05:57 +0300

    Initial commit

---
+++ VirtualizingStackPanelScaleTransform/MainWindow.axaml.cs
@@ -0,0 +1,9 @@
+using Avalonia.Controls;
+
+namespace VirtualizingStackPanelScaleTransform;
+
+public partial class MainWindow : Window {
+    public MainWindow() {
+        InitializeComponent();
+    }
+}
