4825dc7583dbc0b053c89511a71fb66965988580
SKProCH <skproch@yandex.ru>
2023-08-06 02:13:35 +0300

    Add VirtualizingStackPanelScaleTransform repro

--- VirtualizingStackPanelScaleTransform/MainWindow.axaml.cs
+++ VirtualizingStackPanelScaleTransform/MainWindow.axaml.cs
@@ -1,9 +1,18 @@
+using System.Collections.Generic;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 
 namespace VirtualizingStackPanelScaleTransform;
 
 public partial class MainWindow : Window {
     public MainWindow() {
         InitializeComponent();
+    }
+    private void Button_OnClick_AddNewListBox(object? sender, RoutedEventArgs e) {
+        ContentPresenter1.Content = new ListBox() {
+            ItemsSource = new List<object>() {
+                new ListBoxItem() { Content = 1 }
+            }
+        };
     }
 }
