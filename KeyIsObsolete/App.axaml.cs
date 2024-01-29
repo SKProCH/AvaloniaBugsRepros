b1aaaa36c376c0d35f8c439ec77c2b3553e925c2
SKProCH <skproch@yandex.ru>
2024-01-29 18:45:34 +0300

    Creates a new KeyIsObsolete repro

--- KeyIsObsolete/App.axaml.cs
+++ KeyIsObsolete/App.axaml.cs
@@ -2,7 +2,7 @@ using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
 
-namespace BorderBindingToSolidColorBrush;
+namespace KeyIsObsolete;
 
 public partial class App : Application {
     public override void Initialize() {
---
+++ KeyIsObsolete/Program.cs
@@ -0,0 +1,24 @@
+﻿using Avalonia;
+using System;
+using Avalonia.Styling;
+
+namespace KeyIsObsolete;
+
+class Program {
+    public static ThemeVariant Test1 { get; } = ThemeVariant.Dark;
+    public static ThemeVariant Test2 { get; } = ThemeVariant.Light;
+    
+    // Initialization code. Don't use any Avalonia, third-party APIs or any
+    // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
+    // yet and stuff might break.
+    [STAThread]
+    public static void Main(string[] args) => BuildAvaloniaApp()
+        .StartWithClassicDesktopLifetime(args);
+
+    // Avalonia configuration, don't remove; also used by visual designer.
+    public static AppBuilder BuildAvaloniaApp()
+        => AppBuilder.Configure<App>()
+            .UsePlatformDetect()
+            .WithInterFont()
+            .LogToTrace();
+}
